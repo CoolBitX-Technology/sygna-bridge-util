8a0ec33bcbf6d927836575bdca9402d584b3a4fe
Anton Cheng <anton@coolbitx.com>
2019-07-24 14:39:23 +0800

    Add getVASPList() function.

--- src/api/index.js
+++ src/api/index.js
@@ -2,20 +2,34 @@ const originator = require('./originator');
 const beneficiary = require('./beneficiary');
 
 /**
- * Request specific vasp's public key. Can be called by anyone.
+ * get list of registered VASP associated with publicKey.
+ * @param {string} sygnaBridgeDomain 
+ * @param {string} api_key 
+ * @return {Promise<Array<{ vasp_name:string, vasp_code:string, vasp_pubkey:string }>>}
+ */
+async function getVASPList(){
+    const url = sygnaBridgeDomain + '/v1/get-vasp';
+    const headers = { "api-key": api_key };
+    const vasps = await request.getSygnaBridge(url, headers);
+    return vasps;
+}
+
+/**
+ * A Wrapper function of getVASPList to return specific VASP's publickey.
+ * @param {string} sygnaBridgeDomain
+ * @param {string} api_key
  * @param {string} vasp_code 
  * @return {Promise<string>} uncompressed publickey
  */
 async function getVASPPublicKey(sygnaBridgeDomain, api_key, vasp_code){
-    const url = sygnaBridgeDomain + '/v1/get-vasp';
-    const headers = { "api-key": api_key };
-    const vasps = await request.getSygnaBridge(url, headers);
+    const vasps = await getVASPList(sygnaBridgeDomain, api_key);
     const target = vasps.filter(vasp=>vasp.vasp_code === vasp_code).map(vasp=>vasp.vasp_pubkey);
     if (target.length < 1) throw new Error("Invalid vasp_code");
     return target[0];
 }
 
 module.exports = {
+    getVASPList,
     getVASPPublicKey,
     originator,
     beneficiary,
