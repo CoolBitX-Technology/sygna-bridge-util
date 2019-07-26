f699a1324d146a09f3742069af13333b898841ce
Anton Cheng <anton@coolbitx.com>
2019-07-26 15:44:50 +0800

    Remove ref_id from transfer

--- src/api/originator.js
+++ src/api/originator.js
@@ -8,14 +8,13 @@ const request = require('./request');
  * @param {string} hex_data Private sender info encoded by crypto.sygnaEncodePrivateObj
  * @param {{originator_vasp_code: string, originator_addr:string, beneficiary_vasp_code:string, beneficiary_addr:string, transaction_currency:string, amount:number}} transaction
  * @param {string} originator_signature Signature of {hex_data, transaction} signed with crypto.signObject
- * @param {string} reference_id local id for this tx
  * @param {string} callback_url The url 
  * @return {Promise<string>} transfer-id 
  */
-async function transfer(sygnaBridgeDomain, api_key, hex_data, transaction, originator_signature, reference_id, callback_url) {
+async function transfer(sygnaBridgeDomain, api_key, hex_data, transaction, originator_signature, callback_url) {
     const url = sygnaBridgeDomain + '/v1/transfer';
     const headers = { "api-key": api_key };
-    const params = { hex_data, transaction, originator_signature, reference_id, callback_url };
+    const params = { hex_data, transaction, originator_signature, callback_url };
     const transfer_id = await request.postSygnaBridge(url, headers, params);
     return transfer_id;
 }
