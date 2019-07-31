b0eb8fb682eaefef75f425dae6e1678d390a7fbe
Anton Cheng <anton@coolbitx.com>
2019-07-31 16:11:18 +0800

    To new signing and verifying logic

--- src/crypto/index.js
+++ src/crypto/index.js
@@ -25,24 +25,69 @@ exports.sygnaDecodePrivateObg = (privMsg, privateKey) => {
 };
 
 /**
+ * @param {string} private_info
+ * @param {object} transaction
+ * @param {string} data_dt
+ * @param {string} privateKey
+ * @return {{private_info: string, transaction:{}, data_dt:string, signature:string}}
+ */
+exports.signTransferData = (private_info, transaction, data_dt, privateKey) => {
+    let data = {
+        private_info,
+        transaction,
+        data_dt
+    };
+    return this.signObject(data, privateKey);
+};
+
+/**
+ * @param {string} callback_url
+ * @param {string} privateKey
+ * @return {{callback_url_string, signature: string}}
+ */
+exports.signCallBack = (callback_url, privateKey) => {
+    let data = {
+        callback_url,
+    };
+    return this.signObject(data, privateKey);
+};
+
+/**
+ * @param {string} transfer_id
+ * @param {string} result REJECT or ACCEPT
+ * @param {string} privateKey
+ * @return {{transfer_id:string, result: string, signature: string}}
+ */
+exports.signResult = (transfer_id, result, privateKey) => {
+    let data = {
+        transfer_id,
+        result
+    };
+    return this.signObject(data, privateKey);
+};
+
+/**
  * Sign Objects.
  * @param {object} obj
  * @param {string} privateKey
- * @return {string}
+ * @return {object} original object adding a signature field
  */
 exports.signObject = (obj, privateKey) => {
+    obj.signature = "";
     const msgStr = JSON.stringify(obj);
     const signature = sygnaSign.sign(msgStr, privateKey);
-    return signature.toString('hex');
+    obj.signature = signature.toString('hex');
+    return obj;
 };
 
 /**
  * @param {object} obj
  * @param {string} publicKey
- * @param {string} signature
  * @return {boolean}
  */
-exports.verifyObject = (obj, publicKey, signature) => {
+exports.verifyObject = (obj, publicKey) => {
+    const { signature } = obj;
+    obj.signature = "";
     const msgStr = JSON.stringify(obj);
     return sygnaSign.verify(msgStr, signature, publicKey);
 };
