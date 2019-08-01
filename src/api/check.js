1946e0e8089c72493612b66d9c7841f5494c494b
Anton Cheng <anton@coolbitx.com>
2019-08-01 15:11:28 +0800

    add obj signature check

---
+++ src/api/check.js
@@ -0,0 +1,13 @@
+/**
+ * @param {{signature:string}} obj 
+ * @return {void}
+ */
+function checkObjSigned (obj){
+    if(!obj.signature) throw new Error(`Missing signature in Object`);
+    if(typeof obj.signature != "string") throw new Error(`Expect signature to be string, got ${typeof obj.signature}`);
+    if(obj.signature.length != 128 ) throw new Error("Expect signature length to be 128.");
+}
+
+module.exports = {
+    checkObjSigned
+};
