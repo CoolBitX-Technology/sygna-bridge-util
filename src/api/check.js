c71b6a7b537a98219481a454fd21e20fa80744f2
kunming.liu <kunming@coolbitx.com>
2020-02-19 11:07:46 +0800

    added checkObjSigned test

--- src/api/check.js
+++ src/api/check.js
@@ -3,20 +3,20 @@ const EXPIRE_DATE_MIN_OFFSET = 60;
  * @param {{signature:string}} obj 
  * @return {void}
  */
-function checkObjSigned (obj){
-    if(!obj.signature) throw new Error(`Missing signature in Object`);
-    if(typeof obj.signature !== "string") throw new Error(`Expect signature to be string, got ${typeof obj.signature}`);
-    if(obj.signature.length !== 128 ) throw new Error("Expect signature length to be 128.");
+function checkObjSigned(obj) {
+    if (!obj || !obj.signature) throw new Error(`Missing signature in Object`);
+    if (typeof obj.signature !== "string") throw new Error(`Expect signature to be string, got ${typeof obj.signature}`);
+    if (obj.signature.length !== 128) throw new Error("Expect signature length to be 128.");
 }
 
-function checkExpireDateValid(expire_date){
-    if(!expire_date)
+function checkExpireDateValid(expire_date) {
+    if (!expire_date)
         return;
-        
-    if(typeof expire_date !== "number") throw new Error(`Expect expire_date to be number, got ${typeof expire_date}`);
+
+    if (typeof expire_date !== "number") throw new Error(`Expect expire_date to be number, got ${typeof expire_date}`);
     const date = new Date(expire_date)
     const today = new Date()
-    if( date.getTime() - today.getTime() < EXPIRE_DATE_MIN_OFFSET ){
+    if (date.getTime() - today.getTime() < EXPIRE_DATE_MIN_OFFSET) {
         throw new Error('expire_date should be at least 60 seconds away from the current time.');
     }
 }
--- test/check.test.js
+++ test/check.test.js
@@ -1,58 +1,94 @@
 const {
+  checkObjSigned,
   checkExpireDateValid
 } = require('../src/api/check');
 
 describe('test check', () => {
+  describe('test checkObjSigned', () => {
+    it('should throw error if signature is not valid', () => {
+      expect(() => {
+        checkObjSigned("123");
+      }).toThrow(new Error(`Missing signature in Object`));
+
+      expect(() => {
+        checkObjSigned(123);
+      }).toThrow(new Error(`Missing signature in Object`));
+
+      expect(() => {
+        checkObjSigned(
+          { a: 1 }
+        );
+      }).toThrow(new Error(`Missing signature in Object`));
+
+      expect(() => {
+        checkObjSigned();
+      }).toThrow(new Error(`Missing signature in Object`));
+
+      expect(() => {
+        checkObjSigned({
+          signature: 123
+        });
+      }).toThrow(new Error(`Expect signature to be string, got number`));
+
+      expect(() => {
+        checkObjSigned({
+          signature: { a: 1 }
+        });
+      }).toThrow(new Error(`Expect signature to be string, got object`));
+
+      expect(() => {
+        checkObjSigned({
+          signature: "123"
+        });
+      }).toThrow(new Error(`Expect signature length to be 128.`));
+
+      expect(() => {
+        checkObjSigned({
+          signature: "359f313230b12e3bc00af64a5462026a4a29ff1be2fadf1c03012c7aec96f10ded5c37a27d38906345f2066ee3e6c94b323b5dec0f396333d7e116b8d81e8fc7"
+        })
+      }).not.toThrow()
+
+    });
+  });
   describe('test checkExpireDateValid', () => {
     it('should throw error if expire_date is not number', () => {
-      try {
+      expect(() => {
         checkExpireDateValid("123");
-      } catch (error) {
-        expect(error).toEqual(new Error(`Expect expire_date to be number, got string`))
-      }
+      }).toThrow(new Error(`Expect expire_date to be number, got string`));
 
-      try {
+      expect(() => {
         checkExpireDateValid(true);
-      } catch (error) {
-        expect(error).toEqual(new Error(`Expect expire_date to be number, got boolean`))
-      }
+      }).toThrow(new Error(`Expect expire_date to be number, got boolean`));
 
-      try {
-        checkExpireDateValid({a:1});
-      } catch (error) {
-        expect(error).toEqual(new Error(`Expect expire_date to be number, got object`))
-      }
+      expect(() => {
+        checkExpireDateValid({ a: 1 });
+      }).toThrow(new Error(`Expect expire_date to be number, got object`));
     });
 
     it('should throw error if expire_date is too short', () => {
-      try {
+      expect(() => {
         checkExpireDateValid(1582011631);//GMT: Tuesday, February 18, 2020 7:40:31 AM
-      } catch (error) {
-        expect(error).toEqual(new Error(`expire_date should be at least 60 seconds away from the current time.`))
-      }
+      }).toThrow(new Error(`expire_date should be at least 60 seconds away from the current time.`));
 
-      try {
+      expect(() => {
         checkExpireDateValid(1582011631000);//GMT: Tuesday, February 18, 2020 7:40:31 AM
-      } catch (error) {
-        expect(error).toEqual(new Error(`expire_date should be at least 60 seconds away from the current time.`))
-      }
+      }).toThrow(new Error(`expire_date should be at least 60 seconds away from the current time.`));
 
-      try {
+      expect(() => {
         checkExpireDateValid(4106604075);//GMT: Thursday, February 18, 2100 3:21:15 AM
-      } catch (error) {
-        expect(error).toEqual(new Error(`expire_date should be at least 60 seconds away from the current time.`))
-      }
+      }).toThrow(new Error(`expire_date should be at least 60 seconds away from the current time.`));
 
-      expect(function() {
+
+      expect(function () {
         checkExpireDateValid();
         checkExpireDateValid(undefined);
         checkExpireDateValid(false);
         checkExpireDateValid(0);
         checkExpireDateValid(4106604075000);//GMT: Thursday, February 18, 2100 3:21:15 AM
       }).not.toThrow();
-  
+
     });
-    
+
   });
 
 });
