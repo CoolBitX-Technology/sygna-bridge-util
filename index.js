f77762fcbc346e228726a2b715d5d3f4a06fd433
kunming.liu <kunming@coolbitx.com>
2020-02-25 21:03:55 +0800

    fixed wrong path

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const crypto = require('./src/crypto');
 const API = require('./src/api').API;
 const config = require('./src/config');
-const { validateSchema } = require('./src/utils');
+const { validateSchema } = require('./src/utils/validateSchema');
+
 module.exports = {
     API,
     crypto,
