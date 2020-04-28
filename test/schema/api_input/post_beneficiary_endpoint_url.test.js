0328e01483617265dd1e5de9966b0a9fc6cf3ab1
kunming.liu <kunming@coolbitx.com>
2020-04-28 11:11:43 +0800

    revise beneficiary_endpoint_url schemas

--- test/schema/api_input/post_beneficiary_endpoint_url.test.js
+++ test/schema/api_input/post_beneficiary_endpoint_url.test.js
@@ -4,7 +4,9 @@ const {
 
 describe('test validate post_beneficiary_endpoint_url', () => {
   const vasp_code = 'QQQQKRQQ';
-  const beneficiary_endpoint_url = 'https://api.sygna.io/api/v1.1.0/bridge/';
+  const callback_permission_request_url =
+    'https://api.sygna.io/api/v1.1.0/bridge/permission-request';
+  const callback_txid_url = 'https://api.sygna.io/api/v1.1.0/bridge/txid';
   const signature =
     '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b';
 
@@ -14,7 +16,7 @@ describe('test validate post_beneficiary_endpoint_url', () => {
     expect(valid[0]).toBe(false);
     const { dataPath, message } = valid[1][0];
     expect(dataPath).toEqual('');
-    expect(message).toEqual("should have required property 'vasp_code'");
+    expect(message).toEqual("should have required property '.vasp_code'");
 
     data.vasp_code = 123;
     const valid1 = validatePostBeneficiaryEndpointUrlSchema(data);
@@ -31,41 +33,96 @@ describe('test validate post_beneficiary_endpoint_url', () => {
     expect(message2).toEqual('should NOT be shorter than 1 characters');
   });
 
-  it('should validate failed if beneficiary_endpoint_url is not valid', () => {
+  it('should validate failed if callback_permission_request_url and callback_txid_url are empty', () => {
     const data = { vasp_code };
     const valid = validatePostBeneficiaryEndpointUrlSchema(data);
     expect(valid[0]).toBe(false);
-    const { dataPath, message } = valid[1][0];
+    const messages = valid[1];
+    expect(messages.length).toBe(3);
+    const { dataPath, message } = messages[0];
     expect(dataPath).toEqual('');
     expect(message).toEqual(
-      "should have required property 'beneficiary_endpoint_url'",
+      "should have required property '.callback_permission_request_url'",
     );
 
-    data.beneficiary_endpoint_url = 123;
-    const valid1 = validatePostBeneficiaryEndpointUrlSchema(data);
-    expect(valid1[0]).toBe(false);
-    const { dataPath: dataPath1, message: message1 } = valid1[1][0];
-    expect(dataPath1).toEqual('.beneficiary_endpoint_url');
+    const { dataPath: dataPath1, message: message1 } = messages[1];
+    expect(dataPath1).toEqual('');
+    expect(message1).toEqual(
+      "should have required property '.callback_txid_url'",
+    );
+
+    const { dataPath: dataPath2, message: message2 } = messages[2];
+    expect(dataPath2).toEqual('');
+    expect(message2).toEqual('should match some schema in anyOf');
+  });
+
+  it('should validate failed if callback_permission_request_url is not valid', () => {
+    const data = { vasp_code };
+    data.callback_permission_request_url = 123;
+    const valid = validatePostBeneficiaryEndpointUrlSchema(data);
+    expect(valid[0]).toBe(false);
+    const { dataPath: dataPath1, message: message1 } = valid[1][0];
+    expect(dataPath1).toEqual('.callback_permission_request_url');
     expect(message1).toEqual('should be string');
 
-    data.beneficiary_endpoint_url = 'abcde';
-    const valid2 = validatePostBeneficiaryEndpointUrlSchema(data);
-    expect(valid2[0]).toBe(false);
-    const { dataPath: dataPath2, message: message2 } = valid2[1][0];
-    expect(dataPath2).toEqual('.beneficiary_endpoint_url');
+    data.callback_permission_request_url = 'abcde';
+    const valid1 = validatePostBeneficiaryEndpointUrlSchema(data);
+    expect(valid1[0]).toBe(false);
+    const { dataPath: dataPath2, message: message2 } = valid1[1][0];
+    expect(dataPath2).toEqual('.callback_permission_request_url');
     expect(message2).toEqual('should match format "uri"');
+  });
+
+  it('should validate failed if callback_txid_url is not valid', () => {
+    const data = { vasp_code };
+    data.callback_txid_url = 123;
+    const valid = validatePostBeneficiaryEndpointUrlSchema(data);
+    expect(valid[0]).toBe(false);
+    const messages = valid[1];
+    expect(messages.length).toBe(3);
+
+    const { dataPath, message } = messages[0];
+    expect(dataPath).toEqual('.callback_txid_url');
+    expect(message).toEqual('should be null');
+
+    const { dataPath: dataPath1, message: message1 } = messages[1];
+    expect(dataPath1).toEqual('.callback_txid_url');
+    expect(message1).toEqual('should be string');
+
+    const { dataPath: dataPath2, message: message2 } = messages[2];
+    expect(dataPath2).toEqual('.callback_txid_url');
+    expect(message2).toEqual('should match exactly one schema in oneOf');
+
+    data.callback_txid_url = 'abcde';
+    const valid1 = validatePostBeneficiaryEndpointUrlSchema(data);
+    expect(valid1[0]).toBe(false);
+
+    const messages_1 = valid1[1];
+    expect(messages_1.length).toBe(3);
+
+    const { dataPath: dataPath_1, message: message_1 } = messages_1[0];
+    expect(dataPath_1).toEqual('.callback_txid_url');
+    expect(message_1).toEqual('should be null');
+
+    const { dataPath: dataPath_2, message: message_2 } = messages_1[1];
+    expect(dataPath_2).toEqual('.callback_txid_url');
+    expect(message_2).toEqual('should match format "uri"');
+
+    const { dataPath: dataPath_3, message: message_3 } = messages_1[2];
+    expect(dataPath_3).toEqual('.callback_txid_url');
+    expect(message_3).toEqual('should match exactly one schema in oneOf');
   });
 
   it('should validate failed if signature is not valid', () => {
     const data = {
       vasp_code,
-      beneficiary_endpoint_url,
+      callback_permission_request_url,
     };
     const valid = validatePostBeneficiaryEndpointUrlSchema(data);
     expect(valid[0]).toBe(false);
     const { dataPath, message } = valid[1][0];
     expect(dataPath).toEqual('');
-    expect(message).toEqual("should have required property 'signature'");
+    expect(message).toEqual("should have required property '.signature'");
 
     data.signature = 0;
     const valid2 = validatePostBeneficiaryEndpointUrlSchema(data);
@@ -101,10 +158,44 @@ describe('test validate post_beneficiary_endpoint_url', () => {
   it('should validate success', () => {
     const data = {
       vasp_code,
-      beneficiary_endpoint_url,
+      callback_permission_request_url,
       signature,
     };
     const valid = validatePostBeneficiaryEndpointUrlSchema(data);
     expect(valid[0]).toBe(true);
+
+    const data1 = {
+      vasp_code,
+      callback_txid_url,
+      signature,
+    };
+    const valid1 = validatePostBeneficiaryEndpointUrlSchema(data1);
+    expect(valid1[0]).toBe(true);
+
+    const data2 = {
+      vasp_code,
+      callback_permission_request_url,
+      callback_txid_url,
+      signature,
+    };
+    const valid2 = validatePostBeneficiaryEndpointUrlSchema(data2);
+    expect(valid2[0]).toBe(true);
+
+    const data3 = {
+      vasp_code,
+      signature,
+      callback_txid_url: null,
+    };
+    const valid3 = validatePostBeneficiaryEndpointUrlSchema(data3);
+    expect(valid3[0]).toBe(true);
+
+    const data4 = {
+      signature,
+      vasp_code,
+      callback_permission_request_url,
+      callback_txid_url: null,
+    };
+    const valid4 = validatePostBeneficiaryEndpointUrlSchema(data4);
+    expect(valid4[0]).toBe(true);
   });
 });
--- test/schema/data/beneficiary_endpoint_url.test.js
+++ test/schema/data/beneficiary_endpoint_url.test.js
@@ -2,9 +2,11 @@ const {
   validateBeneficiaryEndpointUrlSchema,
 } = require('../../../src/utils/validateSchema');
 
-describe('test validate beneficiary_endpoint_url_schema', () => {
+describe('test validate callback_permission_request_url_schema', () => {
   const vasp_code = 'QQQQKRQQ';
-  const beneficiary_endpoint_url = 'https://api.sygna.io/api/v1.1.0/bridge/';
+  const callback_permission_request_url =
+    'https://api.sygna.io/api/v1.1.0/bridge/permission-request';
+  const callback_txid_url = 'https://api.sygna.io/api/v1.1.0/bridge/txid';
 
   it('should validate failed if vasp_code is not valid', () => {
     const data = {};
@@ -12,9 +14,10 @@ describe('test validate beneficiary_endpoint_url_schema', () => {
     expect(valid[0]).toBe(false);
     const { dataPath, message } = valid[1][0];
     expect(dataPath).toEqual('');
-    expect(message).toEqual("should have required property 'vasp_code'");
+    expect(message).toEqual("should have required property '.vasp_code'");
 
     data.vasp_code = 123;
+    data.callback_permission_request_url = callback_permission_request_url;
     const valid1 = validateBeneficiaryEndpointUrlSchema(data);
     expect(valid1[0]).toBe(false);
     const { dataPath: dataPath1, message: message1 } = valid1[1][0];
@@ -29,37 +32,122 @@ describe('test validate beneficiary_endpoint_url_schema', () => {
     expect(message2).toEqual('should NOT be shorter than 1 characters');
   });
 
-  it('should validate failed if beneficiary_endpoint_url is not valid', () => {
+  it('should validate failed if callback_permission_request_url and callback_txid_url are empty', () => {
     const data = { vasp_code };
     const valid = validateBeneficiaryEndpointUrlSchema(data);
     expect(valid[0]).toBe(false);
-    const { dataPath, message } = valid[1][0];
+    const messages = valid[1];
+    expect(messages.length).toBe(3);
+    const { dataPath, message } = messages[0];
     expect(dataPath).toEqual('');
     expect(message).toEqual(
-      "should have required property 'beneficiary_endpoint_url'",
+      "should have required property '.callback_permission_request_url'",
     );
 
-    data.beneficiary_endpoint_url = 123;
+    const { dataPath: dataPath1, message: message1 } = messages[1];
+    expect(dataPath1).toEqual('');
+    expect(message1).toEqual(
+      "should have required property '.callback_txid_url'",
+    );
+
+    const { dataPath: dataPath2, message: message2 } = messages[2];
+    expect(dataPath2).toEqual('');
+    expect(message2).toEqual('should match some schema in anyOf');
+  });
+
+  it('should validate failed if callback_permission_request_url is not valid', () => {
+    const data = { vasp_code };
+    data.callback_permission_request_url = 123;
+    const valid = validateBeneficiaryEndpointUrlSchema(data);
+    expect(valid[0]).toBe(false);
+    const { dataPath, message } = valid[1][0];
+    expect(dataPath).toEqual('.callback_permission_request_url');
+    expect(message).toEqual('should be string');
+
+    data.callback_permission_request_url = 'abcde';
     const valid1 = validateBeneficiaryEndpointUrlSchema(data);
     expect(valid1[0]).toBe(false);
     const { dataPath: dataPath1, message: message1 } = valid1[1][0];
-    expect(dataPath1).toEqual('.beneficiary_endpoint_url');
+    expect(dataPath1).toEqual('.callback_permission_request_url');
+    expect(message1).toEqual('should match format "uri"');
+  });
+
+  it('should validate failed if callback_txid_url is not valid', () => {
+    const data = { vasp_code };
+    data.callback_txid_url = 123;
+    const valid = validateBeneficiaryEndpointUrlSchema(data);
+    expect(valid[0]).toBe(false);
+    const messages = valid[1];
+    expect(messages.length).toBe(3);
+
+    const { dataPath, message } = messages[0];
+    expect(dataPath).toEqual('.callback_txid_url');
+    expect(message).toEqual('should be null');
+
+    const { dataPath: dataPath1, message: message1 } = messages[1];
+    expect(dataPath1).toEqual('.callback_txid_url');
     expect(message1).toEqual('should be string');
 
-    data.beneficiary_endpoint_url = 'abcde';
-    const valid2 = validateBeneficiaryEndpointUrlSchema(data);
-    expect(valid2[0]).toBe(false);
-    const { dataPath: dataPath2, message: message2 } = valid2[1][0];
-    expect(dataPath2).toEqual('.beneficiary_endpoint_url');
-    expect(message2).toEqual('should match format "uri"');
+    const { dataPath: dataPath2, message: message2 } = messages[2];
+    expect(dataPath2).toEqual('.callback_txid_url');
+    expect(message2).toEqual('should match exactly one schema in oneOf');
+
+    data.callback_txid_url = 'abcde';
+    const valid1 = validateBeneficiaryEndpointUrlSchema(data);
+    expect(valid1[0]).toBe(false);
+
+    const messages_1 = valid1[1];
+    expect(messages_1.length).toBe(3);
+
+    const { dataPath: dataPath_1, message: message_1 } = messages_1[0];
+    expect(dataPath_1).toEqual('.callback_txid_url');
+    expect(message_1).toEqual('should be null');
+
+    const { dataPath: dataPath_2, message: message_2 } = messages_1[1];
+    expect(dataPath_2).toEqual('.callback_txid_url');
+    expect(message_2).toEqual('should match format "uri"');
+
+    const { dataPath: dataPath_3, message: message_3 } = messages_1[2];
+    expect(dataPath_3).toEqual('.callback_txid_url');
+    expect(message_3).toEqual('should match exactly one schema in oneOf');
   });
 
   it('should validate success', () => {
     const data = {
       vasp_code,
-      beneficiary_endpoint_url,
+      callback_permission_request_url,
     };
     const valid = validateBeneficiaryEndpointUrlSchema(data);
     expect(valid[0]).toBe(true);
+
+    const data1 = {
+      vasp_code,
+      callback_txid_url,
+    };
+    const valid1 = validateBeneficiaryEndpointUrlSchema(data1);
+    expect(valid1[0]).toBe(true);
+
+    const data2 = {
+      vasp_code,
+      callback_permission_request_url,
+      callback_txid_url,
+    };
+    const valid2 = validateBeneficiaryEndpointUrlSchema(data2);
+    expect(valid2[0]).toBe(true);
+
+    const data3 = {
+      vasp_code,
+      callback_txid_url: null,
+    };
+    const valid3 = validateBeneficiaryEndpointUrlSchema(data3);
+    expect(valid3[0]).toBe(true);
+
+    const data4 = {
+      vasp_code,
+      callback_permission_request_url,
+      callback_txid_url: null,
+    };
+    const valid4 = validateBeneficiaryEndpointUrlSchema(data4);
+    expect(valid4[0]).toBe(true);
   });
 });
